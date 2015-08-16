f15436d2034c883046c32b523e9ab4daac7a457a
Christoph <gutentach@unitybox.de>
2015-08-16 09:42:54 +0200

    Introduce AUDIO_LEGACY_HACK to extend support for legacy devices
    Conflicts:
    	services/audioflinger/Threads.cpp
    
    Change-Id: Id04a27bb11f97de2ac716ffa3f669c3b6d83612d

--- services/audioflinger/Threads.cpp
+++ services/audioflinger/Threads.cpp
@@ -2057,6 +2057,7 @@ void AudioFlinger::PlaybackThread::readOutputParameters_l()
                 mFrameCount);
     }
 
+#ifndef AUDIO_LEGACY_HACK
     if ((mOutput->flags & AUDIO_OUTPUT_FLAG_NON_BLOCKING) &&
             (mOutput->stream->set_callback != NULL)) {
         if (mOutput->stream->set_callback(mOutput->stream,
@@ -2065,6 +2066,7 @@ void AudioFlinger::PlaybackThread::readOutputParameters_l()
             mCallbackThread = new AudioFlinger::AsyncCallbackThread(this);
         }
     }
+#endif
 
     mHwSupportsPause = false;
     if (mOutput->flags & AUDIO_OUTPUT_FLAG_DIRECT) {
@@ -2399,6 +2401,7 @@ ssize_t AudioFlinger::PlaybackThread::threadLoop_write()
 
 void AudioFlinger::PlaybackThread::threadLoop_drain()
 {
+#ifndef AUDIO_LEGACY_HACK
     if (mOutput->stream->drain) {
         ALOGV("draining %s", (mMixerStatus == MIXER_DRAIN_TRACK) ? "early" : "full");
         if (mUseAsyncWrite) {
@@ -2411,6 +2414,7 @@ void AudioFlinger::PlaybackThread::threadLoop_drain()
             (mMixerStatus == MIXER_DRAIN_TRACK) ? AUDIO_DRAIN_EARLY_NOTIFY
                                                 : AUDIO_DRAIN_ALL);
     }
+#endif
 }
 
 void AudioFlinger::PlaybackThread::threadLoop_exit()
@@ -2996,6 +3000,7 @@ status_t AudioFlinger::PlaybackThread::getTimestamp_l(AudioTimestamp& timestamp)
     if (mNormalSink != 0) {
         return mNormalSink->getTimestamp(timestamp);
     }
+#ifndef AUDIO_LEGACY_HACK
     if ((mType == OFFLOAD || mType == DIRECT)
             && mOutput != NULL && mOutput->stream->get_presentation_position) {
         uint64_t position64;
@@ -3006,6 +3011,7 @@ status_t AudioFlinger::PlaybackThread::getTimestamp_l(AudioTimestamp& timestamp)
             return NO_ERROR;
         }
     }
+#endif
     return INVALID_OPERATION;
 }
 
@@ -4876,13 +4882,17 @@ AudioFlinger::OffloadThread::OffloadThread(const sp<AudioFlinger>& audioFlinger,
 
 void AudioFlinger::OffloadThread::threadLoop_exit()
 {
+#ifndef AUDIO_LEGACY_HACK
     if (mFlushPending || mHwPaused) {
         // If a flush is pending or track was paused, just discard buffered data
         flushHw_l();
     } else {
+#endif
         mMixerStatus = MIXER_DRAIN_ALL;
         threadLoop_drain();
+#ifndef AUDIO_LEGACY_HACK
     }
+#endif
     if (mUseAsyncWrite) {
         ALOG_ASSERT(mCallbackThread != 0);
         mCallbackThread->exit();
@@ -5076,6 +5086,7 @@ AudioFlinger::PlaybackThread::mixer_state AudioFlinger::OffloadThread::prepareTr
     // If a flush is pending and a track is active but the HW is not paused, force a HW pause
     // before flush and then resume HW. This can happen in case of pause/flush/resume
     // if resume is received before pause is executed.
+#ifndef AUDIO_LEGACY_HACK
     if (!mStandby && (doHwPause || (mFlushPending && !mHwPaused && (count != 0)))) {
         mOutput->stream->pause(mOutput->stream);
     }
@@ -5086,6 +5097,7 @@ AudioFlinger::PlaybackThread::mixer_state AudioFlinger::OffloadThread::prepareTr
     if (!mStandby && doHwResume) {
         mOutput->stream->resume(mOutput->stream);
     }
+#endif
 
     // remove all the tracks that need to be...
     removeTracks_l(*tracksToRemove);
@@ -5122,6 +5134,7 @@ bool AudioFlinger::OffloadThread::waitingAsyncCallback()
 
 void AudioFlinger::OffloadThread::flushHw_l()
 {
+#ifndef AUDIO_LEGACY_HACK
     DirectOutputThread::flushHw_l();
     // Flush anything still waiting in the mixbuffer
     mCurrentWriteLength = 0;
@@ -5137,6 +5150,7 @@ void AudioFlinger::OffloadThread::flushHw_l()
         mCallbackThread->setWriteBlocked(mWriteAckSequence);
         mCallbackThread->setDraining(mDrainSequence);
     }
+#endif
 }
 
 void AudioFlinger::OffloadThread::onAddNewTrack_l()
